2696e16c47fd134e6bbbb4bf66f88f6a70331202
233325332 <233325332@qq.com>
2023-01-13 11:27:34 +0800

    update LICENSE

--- kk.py
+++ kk.py
@@ -1,3 +1,12 @@
+#    Copyright (c) [2023] [陌北v1]
+#    [KK-CODE] is licensed under Mulan PSL v2.
+#    You can use this software according to the terms and conditions of the Mulan PSL v2. 
+#    You may obtain a copy of Mulan PSL v2 at:
+#                http://license.coscl.org.cn/MulanPSL2 
+#    THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
+#    See the Mulan PSL v2 for more details.  
+
+
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
